e26ab18f321a8a5f2b7f19842f63f7b86c6fdafa
Andres Prato <prato.andres@gmail.com>
2020-05-11 21:42:08 -0400

    Add react, and main files. Add default image.

---
+++ src/DogImage.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import './DogImage.css'
+
+export default class DogImage extends Component {
+    render() {
+        return (
+            <div className="dog-image"></div>
+        )
+    }
+}

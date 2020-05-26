5263e5c88cc399e81fead57b1b139f436c719705
Andres Prato <prato.andres@gmail.com>
2020-05-26 19:33:37 -0400

    Add most visited dropdown.

--- src/Menu.js
+++ src/Menu.js
@@ -3,33 +3,37 @@ import React, { Component } from 'react'
 import Clock from 'react-live-clock'
 import breeds from './resources/breeds.json'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCog, faDog } from '@fortawesome/free-solid-svg-icons'
+import { faCog, faDog, faEye } from '@fortawesome/free-solid-svg-icons'
 import './Menu.css'
 
+// var sites = ["Apple", "Orange", "Coconut"];
+
 export default class DogImage extends Component {
+
+    constructor() {
+        super();
+        this.state = { topSites: []};
+    }
+
+    async componentDidMount() {
+        let response = await getTopSites();
+        this.setState({ topSites: response });
+    }
+
     render() {
         return (
-            <nav class="navbar navbar-expand-lg">
-                {/* <a class="navbar-brand" href="https://www.github.com/pratoa/dog-tab">DogTab</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button> */}
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav mr-auto">
-                        <li class="nav-item active">
+            <nav className="navbar navbar-expand-lg">
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
                             <FontAwesomeIcon icon={faCog} size="2x" color="white" className="icon"/>
-                            {/* <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a> */}
                         </li>
-                        <li class="nav-item dropdown">
+                        <li className="nav-item dropdown">
                             <FontAwesomeIcon icon={faDog} size="2x" color="white" className="icon dropdown-toggle" role="button" data-toggle="dropdown"/>
-                            {/* <a class="nav-link dropdown-toggle fas fa-dog" href="#" id="navbarDropdown" 
-                                role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Breeds
-                            </a> */}
-                            <ul class="dropdown-menu checkbox-menu allow-focus" aria-labelledby="dropdownMenu1">
+                            <ul className="breeds-dropdown dropdown-menu checkbox-menu allow-focus" aria-labelledby="dropdownMenu1">
                                 {
                                     breeds.map(breed =>  
-                                        <li>
+                                        <li key={breed}>
                                             <label>
                                                 <input type="checkbox" value={breed} key={breed}/> {breed}
                                             </label>
@@ -37,6 +41,17 @@ export default class DogImage extends Component {
                                     )
                                 }
                             </ul>
+                        </li>
+                        <li className="nav-tem dropdown">
+                            <FontAwesomeIcon icon={faEye} size="2x" color="white" className="icon" role="button" data-toggle="dropdown"/>
+                            <div className="dropdown-menu site-dropdown">
+                                <h4 class="dropdown-header">Most Visited Sites</h4>
+                                {
+                                   this.state.topSites.map(site => 
+                                        <a className="dropdown-item site-item" value={site.title} key={site.title} href={site.url}> {site.title} </a>
+                                    )
+                                } 
+                            </div>   
                         </li>
                     </ul>
                     {/* <form class="form-inline my-2 my-lg-0">
@@ -51,4 +66,13 @@ export default class DogImage extends Component {
             </nav>
         )
     }
+}
+
+async function getTopSites() {
+    return new Promise(function(resolve, reject) {
+        chrome.topSites.get(function(result) {
+            console.log(result);
+            resolve(result);
+         })
+    });
 }

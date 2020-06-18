1d3ea8041eef161e01b1783522e3cfcf14cbb8df
Andres Prato <prato.andres@gmail.com>
2020-06-17 21:37:38 -0400

    Do some nice refactor.

--- src/background/background.js
+++ src/background/background.js
@@ -1,14 +1,13 @@
 /*global chrome*/
-import getImages from "./helpers";
+import { getInitialImgaes } from "./helpers";
 
 chrome.runtime.onInstalled.addListener(() => {
     // console.log('onInstalled...');
     // create alarm after extension is installed / upgraded
     // chrome.alarms.create('refresh', { periodInMinutes: 1 });
 
-    getImages()
+    // getImages("dog", 1, 30, false);
+    // getImages("dog", 2, 30, false);
+
+    getInitialImgaes();
 });
-  
-function helloWorld() {
-    console.log("Hello, world!");
-}
--- src/background/helpers.js
+++ src/background/helpers.js
@@ -5,36 +5,70 @@ const unsplash = new Unsplash({
     accessKey: "3Z5nmpj96SkBih-eqsEFI5Cl_L1kiwkZFF6FpPen37E"
 });
 
-export default function getImages() {
-    unsplash.search.photos("dog", 1, 30, { orientation: "landscape" })
+function getImages(keyword, page, numberOfPhotos, isCustom) {
+    unsplash.search.photos(keyword, page, numberOfPhotos, { orientation: "landscape" })
     .then(toJson)
     .then(json => {
         if (json && json.results.length > 0) {
+            console.log(json);
             console.log(json.results);
-            saveImagesToStorage(json.results);
+            //save
         }
     })
     .catch(err => {
         console.log(err);
+    })
+}
+
+async function getInitialImgaes() {
+    let shouldGet = await checkDogImages();
+    console.log(shouldGet);
+    if (!shouldGet) {
+        var first30 = await unsplashSearch("dog", 1, 30);
+        var second30 = await unsplashSearch("dog", 2, 30);
+        saveImages(first30.concat(second30));
+    }
+}
+
+async function unsplashSearch(keyword, page, perPage) {
+    return new Promise(function(resolve, reject) {
+        unsplash.search.photos(keyword, page, perPage, { orientation: "landscape" })
+        .then(toJson)
+        .then(json => {
+            if (json && json.results.length > 0) {
+                resolve(json.results);
+            }
+        })
+        .catch(err => {
+            console.log(err);
+        })
     });
 }
 
-function saveImagesToStorage(data) {
+function saveImages(images) {
     var imagesToStore = [];
-    data.forEach(function (image) {
+    images.forEach(function (image) {
         var img = new DogImage(image.urls.regular, image.user)
         imagesToStore.push(img);
     });
 
     if (imagesToStore.length > 0) {
         chrome.storage.local.set({'dogImages': imagesToStore}, function() {
-            console.log('Setting DogImages to: ' + imagesToStore.toString());
+            console.log('Setting DogImages to: ');
+            console.log(imagesToStore);
         });
-
         chrome.storage.local.set({'dogCounter': 0}, function() {
             console.log('Setting DogCounter for the first time to: ' + 0);
         });
     }
+}
+
+async function checkDogImages() {
+    return new Promise(function(resolve, reject) { 
+            chrome.storage.local.get(["dogImages"], function(result) {
+            resolve(result.hasOwnProperty("dogImages"));
+        });
+    });
 }
 
 class DogImage {
@@ -43,3 +77,5 @@ class DogImage {
         this.user = user;
     }
 }
+
+export { getInitialImgaes, getImages }
